246de75fb0528856de835423d647a8719e844c01
Alec Chen <goodneighboralec@gmail.com>
2019-12-04 19:22:00 -0800

    grid setup ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import produce from 'immer';
+
+const numRows = 50;
+const numCols = 50;
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
 
+  const [running, setRunning] = useState(false);
+  // const runSimulation = () =
+  return (
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+        }}
+      >
+        {running ? 'stop' : 'start'}
+      </button>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, j) => (
+            <div
+              key={`${i}-${j}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][j] = grid[i][j] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][j] ? 'pink' : undefined,
+                border: 'solid 1px black'
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
 export default App;
